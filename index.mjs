76272505cd6acbe7913cf24e8939bba35352b876
Riffy <rappersinfan@gmail.com>
2021-05-18 20:53:26 +0200

    initial commit with dashboard api

---
+++ index.mjs
@@ -0,0 +1,78 @@
+import express from "express"
+import ip from "ip"
+import * as alt from "alt-server"
+
+export const acpManager = {
+    startdate: 0,
+    port: 9999,
+    app: undefined,
+    secret: "REPLACETHIS",
+
+    init() {
+        acpManager.app = express();
+        acpManager.app.use(express.json());
+        acpManager.app.use(express.urlencoded({ extended: true }));
+        acpManager.app.listen(acpManager.port, () => {
+            alt.log("ACP Started at : " + acpManager.getAcpAddress());
+            acpManager.startdate = new Date();
+            acpManager.registerListeners();
+        });
+    },
+
+    /**
+     * @description Returns the address of the ACP-Api
+     */
+    getAcpAddress() {
+        return ip.address() + ":" + acpManager.port;
+    },
+
+    /**
+     * @description Adds listeners
+     */
+    registerListeners() {
+        acpManager.addDashboardListener();
+    },
+
+    /**
+     * @description Adds the listener for the dashboard view with uptime and online players.
+     */
+    addDashboardListener() {
+        acpManager.addAcpListener("/acp/dashboard", (req, res) => {
+            const dashboardinfo = {
+                uptime: ((new Date().getTime() - acpManager.startdate.getTime()) / 1000),
+                onlinePlayers: alt.Player.all.length
+            };
+            res.status(200).send(JSON.stringify(dashboardinfo));
+        });
+    },
+
+    /**
+     * addAcpListener
+     * @param  {} url
+     * @param  {} action
+     * @description Adds a Listener for the ACP. Will check if provided token matches the secret
+     */
+    addAcpListener(url, action) {
+        if (acpManager.app) {
+            acpManager.app.get(url, (req, res) => {
+                if (!req.query.token) res.sendStatus(401);
+                if (!acpManager.isValidToken(req.query.token.toString())) {
+                    res.sendStatus(401);
+                }
+                else {
+                    action(req, res);
+                }
+            });
+        }
+    },
+
+    /**
+     * @param  {} t
+     * @description Checks if the provided token string matches the secret of the acp manager
+     */
+    isValidToken(t) {
+        return (acpManager.secret === t);
+    }
+}
+
+acpManager.init();
